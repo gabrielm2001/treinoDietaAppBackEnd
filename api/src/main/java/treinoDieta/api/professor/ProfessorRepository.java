df77aa78247d1fb3ccda160fd827d92daaf0811b
Gabriel Machado <124816401+gabrielm2001@users.noreply.github.com>
2024-01-01 21:47:50 -0300

    feat: Adicionei mais um metodo no repositorio do professor onde buscarei domente os professores com que não foram excluidos

--- api/src/main/java/treinoDieta/api/professor/ProfessorRepository.java
+++ api/src/main/java/treinoDieta/api/professor/ProfessorRepository.java
@@ -1,7 +1,9 @@
 package treinoDieta.api.professor;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProfessorRepository extends JpaRepository<Professor, Long>{
+    Page<Professor> findAllByAtivoTrue(Pageable paginacao);
 }
