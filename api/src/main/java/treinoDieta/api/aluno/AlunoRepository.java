00ced85aa98961748bb8db119b51ea942bce688f
Gabriel Machado <124816401+gabrielm2001@users.noreply.github.com>
2024-01-03 19:24:06 -0300

    feat: Adicionei um atributo no repository do Aluno

--- api/src/main/java/treinoDieta/api/aluno/AlunoRepository.java
+++ api/src/main/java/treinoDieta/api/aluno/AlunoRepository.java
@@ -1,6 +1,10 @@
 package treinoDieta.api.aluno;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import treinoDieta.api.professor.Professor;
 
 public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+    Page<Aluno> findAllByAtivoTrue(Pageable paginacao);
 }
