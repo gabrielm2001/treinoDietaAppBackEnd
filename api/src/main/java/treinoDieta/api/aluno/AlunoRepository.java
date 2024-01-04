570954cc536c35d4c963b71fa9b3433450b70f05
Gabriel Machado <124816401+gabrielm2001@users.noreply.github.com>
2024-01-04 20:45:29 -0300

    feat: Criacao do metodo de listagem de alunos de acordo com o professor no respositorio do aluno

--- api/src/main/java/treinoDieta/api/aluno/AlunoRepository.java
+++ api/src/main/java/treinoDieta/api/aluno/AlunoRepository.java
@@ -3,8 +3,12 @@ package treinoDieta.api.aluno;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import treinoDieta.api.professor.Professor;
 
 public interface AlunoRepository extends JpaRepository<Aluno, Long> {
     Page<Aluno> findAllByAtivoTrue(Pageable paginacao);
+
+    @Query("SELECT a FROM Aluno a WHERE a.professor.id = :id and a.ativo = true")
+    Page<DadosDetalhamentoAluno> findAllByProfessorIdAndAtivoTrue(Pageable paginacao, Long id);
 }
