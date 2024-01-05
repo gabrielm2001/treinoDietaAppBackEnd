6cfb5d748ad74037378b0d913a24ea598aa3300e
Gabriel Machado <124816401+gabrielm2001@users.noreply.github.com>
2024-01-05 18:55:23 -0300

    feat: Criacao do repositorio da ficha

---
+++ api/src/main/java/treinoDieta/api/nonPhysicalEntities/ficha/FichaRepository.java
@@ -0,0 +1,7 @@
+package treinoDieta.api.nonPhysicalEntities.ficha;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FichaRepository extends JpaRepository<Ficha, Long> {
+
+}
