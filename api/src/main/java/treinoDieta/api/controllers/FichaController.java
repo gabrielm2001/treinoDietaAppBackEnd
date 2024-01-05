d80201d74465cb4be13f4759df5e8c699573d681
Gabriel Machado <124816401+gabrielm2001@users.noreply.github.com>
2024-01-05 19:22:55 -0300

    feat: Cricao do controller das fichas e cadastro da mesma

---
+++ api/src/main/java/treinoDieta/api/controllers/FichaController.java
@@ -0,0 +1,36 @@
+package treinoDieta.api.controllers;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+import treinoDieta.api.nonPhysicalEntities.ficha.DadosCadastroFicha;
+import treinoDieta.api.nonPhysicalEntities.ficha.DadosDetalhamentoFicha;
+import treinoDieta.api.nonPhysicalEntities.ficha.Ficha;
+import treinoDieta.api.nonPhysicalEntities.ficha.FichaRepository;
+import treinoDieta.api.physicalEntities.aluno.Aluno;
+import treinoDieta.api.physicalEntities.aluno.AlunoRepository;
+import treinoDieta.api.physicalEntities.professor.DadosDetalhamentoProfessor;
+
+@RestController
+@RequestMapping("/ficha")
+public class FichaController {
+
+    @Autowired
+    private FichaRepository fichaRepository;
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    @PostMapping("/{id}")
+    @Transactional
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroFicha dados, @PathVariable Long id, UriComponentsBuilder uriBuilder){
+        var aluno = alunoRepository.getReferenceById(id);
+        var ficha = fichaRepository.save(new Ficha(dados, aluno));
+        var uri = uriBuilder.path("/ficha/{id}").buildAndExpand(ficha.getId()).toUri();
+
+        return  ResponseEntity.created(uri).body(new DadosDetalhamentoFicha(ficha));
+    }
+}
