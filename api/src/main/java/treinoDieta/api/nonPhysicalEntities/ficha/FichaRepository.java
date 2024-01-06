899b8c39eb9ec1f14bec640302e97940495fe491
Gabriel Machado <124816401+gabrielm2001@users.noreply.github.com>
2024-01-06 12:46:00 -0300

    Feat: Criei um método que retorna uma lista de fichas de acordo com o id do aluno

--- api/src/main/java/treinoDieta/api/nonPhysicalEntities/ficha/FichaRepository.java
+++ api/src/main/java/treinoDieta/api/nonPhysicalEntities/ficha/FichaRepository.java
@@ -1,7 +1,11 @@
 package treinoDieta.api.nonPhysicalEntities.ficha;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Range;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FichaRepository extends JpaRepository<Ficha, Long> {
 
+    Page<Ficha> findAllByAluno_id(Pageable pageable, Long alunoId);
 }
