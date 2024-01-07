f61fea09b48994f3999e02d899492ebc4d8c8875
Gabriel Machado <124816401+gabrielm2001@users.noreply.github.com>
2024-01-07 12:00:18 -0300

    Feat: Criacao da JPA do treino

---
+++ api/src/main/java/treinoDieta/api/nonPhysicalEntities/treino/Treino.java
@@ -0,0 +1,34 @@
+package treinoDieta.api.nonPhysicalEntities.treino;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import treinoDieta.api.nonPhysicalEntities.ficha.Ficha;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Table(name = "treinos")
+@Entity(name = "Treino")
+@EqualsAndHashCode(of = "id")
+public class Treino {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nomeTreino;
+
+    @ManyToOne
+    @JoinColumn(name = "ficha_id")
+    private Ficha ficha;
+
+    private boolean ativo;
+
+    public Treino(DadosCadastroTreino dados, Ficha ficha){
+        this.ativo = true;
+        this.nomeTreino = dados.nomeTreino();
+        this.ficha = ficha;
+    }
+}
