6b5e4c6ed23634dede3fe8dec0cf3c76117803ed
Gabriel Machado <124816401+gabrielm2001@users.noreply.github.com>
2024-01-08 10:47:34 -0300

    Feat: Adicionei o NotNull nas chaves estrangeiras das outras entidades

--- api/src/main/resources/db/migration/V8__create_table_treino.sql
+++ api/src/main/resources/db/migration/V8__create_table_treino.sql
@@ -1,10 +1,8 @@
 CREATE TABLE treinos(
    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
    nome_treino VARCHAR(100) NOT NULL,
-
-    ficha_id bigint unsigned,
-
-    ativo BOOLEAN NOT NULL,
+   ficha_id bigint unsigned NOT NULL,
+   ativo BOOLEAN NOT NULL,
 
    PRIMARY KEY(id),
    FOREIGN KEY(ficha_id) REFERENCES fichas(id)
