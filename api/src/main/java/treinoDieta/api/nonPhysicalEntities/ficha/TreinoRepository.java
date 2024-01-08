0e60c4a6604bc2238a9cf13aab0decab0a7baa96
Gabriel Machado <124816401+gabrielm2001@users.noreply.github.com>
2024-01-08 09:39:33 -0300

    Feat: Criei um metodo no repositorio de treino que retorna todos os treinos ativos em uma pagina

--- api/src/main/java/treinoDieta/api/nonPhysicalEntities/ficha/TreinoRepository.java
+++ api/src/main/java/treinoDieta/api/nonPhysicalEntities/ficha/TreinoRepository.java
@@ -1,7 +1,10 @@
 package treinoDieta.api.nonPhysicalEntities.ficha;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import treinoDieta.api.nonPhysicalEntities.treino.Treino;
 
 public interface TreinoRepository extends JpaRepository<Treino, Long> {
+    Page<Treino> findAllByAtivoTrue(Pageable pageable);
 }
