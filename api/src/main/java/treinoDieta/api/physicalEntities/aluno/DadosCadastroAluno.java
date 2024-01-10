da13525f4c4fc9356ca384ad39178e5d5168a2ee
Gabriel Machado <124816401+gabrielm2001@users.noreply.github.com>
2024-01-10 10:15:41 -0300

    Feat: Adicionei o novas validações para o cadastro de aluno

--- api/src/main/java/treinoDieta/api/physicalEntities/aluno/DadosCadastroAluno.java
+++ api/src/main/java/treinoDieta/api/physicalEntities/aluno/DadosCadastroAluno.java
@@ -3,13 +3,16 @@ package treinoDieta.api.physicalEntities.aluno;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record DadosCadastroAluno(
+        @Email
+        @Pattern(regexp = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$", message = "Email inválido")
         @NotBlank @Email String email,
-        @NotBlank String idade,
+        @Pattern(regexp = "\\d{2,4}", message = "Idade inválida") @NotBlank String idade,
         @NotBlank String nome,
-        @NotBlank String altura,
-        @NotBlank String peso,
+        @Pattern(regexp = "\\d{2,4}", message = "A altura deve estar em centímetros") @NotBlank String altura,
+        @Pattern(regexp = "\\d{2,4}", message = "O peso deve estar em quilos") @NotBlank String peso,
         @NotBlank String projeto,
         @NotNull Objetivo objetivo,
         @NotNull Genero genero
