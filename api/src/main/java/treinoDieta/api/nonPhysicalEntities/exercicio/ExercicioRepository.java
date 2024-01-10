176e2f935fd874adf4ea0ea837660be17a9ebf9b
Gabriel Machado <124816401+gabrielm2001@users.noreply.github.com>
2024-01-10 10:21:47 -0300

    Feat: Criei o repositorio do exercicio

---
+++ api/src/main/java/treinoDieta/api/nonPhysicalEntities/exercicio/ExercicioRepository.java
@@ -0,0 +1,6 @@
+package treinoDieta.api.nonPhysicalEntities.exercicio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExercicioRepository extends JpaRepository<Exercicio, Long> {
+}
