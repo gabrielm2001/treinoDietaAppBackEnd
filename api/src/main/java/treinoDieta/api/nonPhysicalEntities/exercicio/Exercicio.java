e352e9f7bae905fc6e7afa4ce0458076d1ce777f
Gabriel Machado <124816401+gabrielm2001@users.noreply.github.com>
2024-01-10 10:48:28 -0300

    Feat: Criei o metodo de listagem de exercicios de uma ficha

--- api/src/main/java/treinoDieta/api/nonPhysicalEntities/exercicio/Exercicio.java
+++ api/src/main/java/treinoDieta/api/nonPhysicalEntities/exercicio/Exercicio.java
@@ -37,5 +37,6 @@ public class Exercicio {
         this.detalhes = dados.detalhes();
         this.nomeExercicio = dados.nomeExercicio();
         this.treino = treino;
+        this.ativo = true;
     }
 }
