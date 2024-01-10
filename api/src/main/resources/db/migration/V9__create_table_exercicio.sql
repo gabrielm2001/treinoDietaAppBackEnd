45b775a208abfb010a4a183f7490654c571fa242
Gabriel Machado <124816401+gabrielm2001@users.noreply.github.com>
2024-01-10 10:14:43 -0300

    Feat: retirei um campo e modifiquei o nome de um campo

--- api/src/main/resources/db/migration/V9__create_table_exercicio.sql
+++ api/src/main/resources/db/migration/V9__create_table_exercicio.sql
@@ -2,12 +2,12 @@ CREATE TABLE exercicios(
 
     id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
 
-    tempoDeTreino VARCHAR(40) NOT NULL,
+
     series VARCHAR(30) NOT NULL,
     repeticoes VARCHAR(30) NOT NULL,
     tecnica VARCHAR(300) NOT NULL,
     detalhes VARCHAR(300) NOT NULL,
-    nomeExercicio VARCHAR(100) NOT NULL,
+    nome_exercicio VARCHAR(100) NOT NULL,
 
     ativo BOOLEAN NOT NULL,
     treino_id bigint unsigned NOT NULL,
