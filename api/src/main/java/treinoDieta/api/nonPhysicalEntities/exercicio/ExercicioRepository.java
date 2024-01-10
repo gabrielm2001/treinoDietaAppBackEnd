09705648bfb87bcbe8b18327ddb0d9987c2c0cfa
Gabriel Machado <124816401+gabrielm2001@users.noreply.github.com>
2024-01-10 10:45:09 -0300

    Feat: Criei o repositório de exercicio

--- api/src/main/java/treinoDieta/api/nonPhysicalEntities/exercicio/ExercicioRepository.java
+++ api/src/main/java/treinoDieta/api/nonPhysicalEntities/exercicio/ExercicioRepository.java
@@ -1,6 +1,9 @@
 package treinoDieta.api.nonPhysicalEntities.exercicio;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExercicioRepository extends JpaRepository<Exercicio, Long> {
+    Page<Exercicio> findAllByAtivoTrueAndTreinoId(Pageable paginacao, Long id);
 }
