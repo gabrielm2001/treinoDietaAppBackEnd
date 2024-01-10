6c81d2d14346206f3c420bb305a34c4806a65266
Gabriel Machado <124816401+gabrielm2001@users.noreply.github.com>
2024-01-10 10:15:12 -0300

    Feat: Adicionei o nome de um campo

--- api/src/main/resources/db/migration/V8__create_table_treino.sql
+++ api/src/main/resources/db/migration/V8__create_table_treino.sql
@@ -1,5 +1,6 @@
 CREATE TABLE treinos(
    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+   tempo_treino VARCHAR(100) NOT NULL,
    nome_treino VARCHAR(100) NOT NULL,
    ficha_id bigint unsigned NOT NULL,
    ativo BOOLEAN NOT NULL,
