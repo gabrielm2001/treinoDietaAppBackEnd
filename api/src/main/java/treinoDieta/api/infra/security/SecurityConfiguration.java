01b8142107827c0557adff9d9807fa3d0d962a3a
Rafael Pillar <124816401+gabrielm2001@users.noreply.github.com>
2024-02-07 19:02:53 -0300

    feat: Configuranda o metodo de gerenciador de autenticacao

--- api/src/main/java/treinoDieta/api/infra/security/SecurityConfiguration.java
+++ api/src/main/java/treinoDieta/api/infra/security/SecurityConfiguration.java
@@ -3,6 +3,8 @@ package treinoDieta.api.infra.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
@@ -30,6 +32,11 @@ public class SecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration auth) throws Exception{
+        return auth.getAuthenticationManager();
     }
 
 
