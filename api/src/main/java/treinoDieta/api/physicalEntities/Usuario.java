341f7b4be396eaabc26707ac0610f7f01373e6ea
Rafael Pillar <124816401+gabrielm2001@users.noreply.github.com>
2024-02-07 19:33:42 -0300

    feat: Criando a entidade JPA dos usuarios implementando o UserDetails

---
+++ api/src/main/java/treinoDieta/api/physicalEntities/Usuario.java
@@ -0,0 +1,78 @@
+package treinoDieta.api.physicalEntities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import treinoDieta.api.physicalEntities.usuario.UsuarioRole;
+
+import java.util.Collection;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@EqualsAndHashCode(of = "id")
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+public class Usuario implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    private String login;
+    private String senha;
+
+    private UsuarioRole role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (role == UsuarioRole.ROLE_ADMIN){
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_PROFESSOR"), new SimpleGrantedAuthority("ROLE_ALUNO"));
+        }
+
+        if (role == UsuarioRole.ROLE_PROFESSOR){
+            return List.of(new SimpleGrantedAuthority("ROLE_PROFESSOR"));
+        }
+
+        if (role == UsuarioRole.ROLE_ALUNO){
+            return List.of(new SimpleGrantedAuthority("ROLE_ALUNO"));
+        }
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
