33ffde0c450a6dc099413908bb0bea54728300be
Rafael Pillar <124816401+gabrielm2001@users.noreply.github.com>
2024-02-08 13:46:21 -0300

    fea: Adicionando mais controles de acesso

--- api/src/main/java/treinoDieta/api/infra/security/SecurityConfiguration.java
+++ api/src/main/java/treinoDieta/api/infra/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package treinoDieta.api.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -12,22 +13,29 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration {
 
+    @Autowired
+    private SecurityFilter securityFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         return httpSecurity.csrf(csrf->csrf.disable())
                 .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth->auth
-                        .requestMatchers(HttpMethod.POST, "/alunos").hasAuthority("ROLE_PROFESSOR")
-                        .requestMatchers(HttpMethod.POST, "/professores").hasAuthority("ROLE_ADMIN")
-                )
+                .authorizeHttpRequests(auth->auth.
+                        requestMatchers(new AntPathRequestMatcher("/auth/login", HttpMethod.POST.name())).permitAll()
+                                .requestMatchers (new AntPathRequestMatcher("/auth/register", HttpMethod.POST.name())).permitAll()
+                                .requestMatchers (new AntPathRequestMatcher("/professor", HttpMethod.POST.name())).hasAuthority("ROLE_ADMIN")
+                        .requestMatchers (new AntPathRequestMatcher("/professor", HttpMethod.GET.name())).hasAuthority("ROLE_ADMIN")
+                        .anyRequest().authenticated()
+                ).addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
-
 
     @Bean
     public PasswordEncoder passwordEncoder(){
