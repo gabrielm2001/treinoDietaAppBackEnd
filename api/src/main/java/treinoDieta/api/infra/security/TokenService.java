f94170f164aa981e3f3e47c5143375cf29e70cf2
Rafael Pillar <124816401+gabrielm2001@users.noreply.github.com>
2024-02-07 22:31:44 -0300

    feat: Adicinado o metodo de verificar o token e retornar o subject de acordo com o token fornecido

--- api/src/main/java/treinoDieta/api/infra/security/TokenService.java
+++ api/src/main/java/treinoDieta/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package treinoDieta.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import treinoDieta.api.physicalEntities.usuario.Usuario;
@@ -23,7 +24,21 @@ public class TokenService {
                     .withClaim("senha", usuario.getSenha())
                     .sign(algorithm);
         } catch (JWTCreationException exception){
-            throw new RuntimeException();
+            throw new RuntimeException("Token inválido");
         }
     }
+
+    public String getSubject(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("dietaTreino_api")
+                    .build().verify(token).getSubject();
+
+        } catch (JWTVerificationException exception){
+            return null;
+        }
+
+    }
+
 }
